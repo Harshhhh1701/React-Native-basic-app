96bb833f552696714521661aed25e6fc4b5ca76a
Harsh <valambeharsh1701@gmail.com>
2022-12-06 03:08:25 +0530

    commit

--- App.js
+++ App.js
@@ -4,8 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={style.boldText}>
+          Hello World
+        </Text>
+      </View>
+      <View style={styles.body}>
+        <Text >Lorem ipsum harsh sit amet</Text>
+        <Text >Lorem ipsum harsh sit amet</Text>
+        <Text >Lorem ipsum harsh sit amet</Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +25,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    backgroundColor:'pink',
+    padding:20,
+  },boldText:{
+    fontWeight:'bold',
+  },
+  body:{
+    backgroundColor:'yellow',
+
+  }
 });
