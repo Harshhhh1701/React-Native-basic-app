a797d703c56d121f29702bd9842a5f795cb3bb74
Harsh <valambeharsh1701@gmail.com>
2022-12-08 00:09:55 +0530

    addon5

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,Button,TextInput } from 'react-native';
+import React, {useState} from 'react';
 export default function App() {
+  const[name,setName]=useState('Shaun');
+  const[person,setPerson]=useState({name:'Mario',age:40})
+  const[age,setAge]=useState('20');
+  const clickHandler=()=>{
+    setName('Harsh');
+    setPerson({name:'Harsh',age:20})
+    
+  }
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={style.boldText}>
+      {/* <View style={styles.header}>
+        <Text style={styles.boldText}>
           Hello World
         </Text>
       </View>
@@ -13,6 +21,16 @@ export default function App() {
         <Text >Lorem ipsum harsh sit amet</Text>
         <Text >Lorem ipsum harsh sit amet</Text>
         <Text >Lorem ipsum harsh sit amet</Text>
+      </View> */}
+      {/* <Text>My name is {name}</Text> */}
+
+
+
+
+      <Text>His name is {name} and his age is {age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler}/>
+
       </View>
     </View>
   );
@@ -34,5 +52,8 @@ const styles = StyleSheet.create({
   body:{
     backgroundColor:'yellow',
 
+  },
+  buttonContainer:{
+    marginTop:20
   }
 });
