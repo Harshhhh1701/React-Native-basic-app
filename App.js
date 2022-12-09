d38fa718d55c2d28e41281856d613fa451eaea9e
Harsh <valambeharsh1701@gmail.com>
2022-12-09 10:57:23 +0530

    addon10

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button,TextInput,FlatList } from 'react-native';
 import React, {useState} from 'react';
 import Header from './components/header';
+import TodoItem from './components/todoitem';
 export default function App() {
   const[todos,setTodos]=useState([
     {text:'buy coffee',key:'1'},
     {text:'complete da',key:'2'},
     {text:'Create an app',key:'3'}
   ])
-
+  const pressHandler=(key)=>{
+    setTodos((prevTodos)=>{
+      return prevTodos.filter(todo=>todo.key!=key)
+    })
+  }
    return(
     <View style={styles.container}>
       <Header/>
@@ -17,7 +22,7 @@ export default function App() {
               <FlatList 
                 data={todos}
                 renderItem={(item)=>(
-                  <Text>{item.text}</Text>
+                  <TodoItem item={item} pressHandler={pressHandler}/>
                 )}
               />
             </View>
