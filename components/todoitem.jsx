a31a4ca086c9f7ce2cdd634a53729406e240b9b7
Harsh <valambeharsh1701@gmail.com>
2022-12-10 10:39:05 +0530

    addon14

--- components/todoitem.jsx
+++ components/todoitem.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View,Button,TextInput,FlatList ,TouchableOpacity} from 'react-native';
-
+import {MaterialIcons} from '@expo/vector-icons'
 export default function TodoItem({item,pressHandler}){
     return(
         <TouchableOpacity onPress={()=>pressHandler(item.key)}>
-            <Text style={styles.item}>
-                {item.text}
-            </Text>
+            <View style={styles.item}>
+                <MaterialIcons name='delete' size={18} color='#333'/>
+                <Text style={styles.itemText}>
+                    {item.text}
+                </Text>
+            </View>
+            
         </TouchableOpacity>
     )
 }
@@ -14,11 +18,15 @@ export default function TodoItem({item,pressHandler}){
 
 const styles=StyleSheet.create({
     item:{
+        flexDirection:'row',
         padding:16,
         marginTop:16,
         borderColor:'#bbb',
         borderWidth:1,
         borderStyle:'dashed',
         borderRadius:10,
+    },
+    itemText:{
+        marginLeft:10,
     }
 })
